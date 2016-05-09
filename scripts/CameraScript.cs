ffc954087988ae4e6550f8d66960aab46e3b52be
Alexander Chuvashov <chuvashovalexander@gmail.com>
2016-05-09 17:55:21 +0500

    Load files

---
+++ scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject rogalik;
+    public float stopPosition = -27;
+    // Update is called once per frame
+
+    void LateUpdate()
+    {
+        if (rogalik != null)
+        {
+            if (rogalik.GetComponent<EventScript>().isVisible)
+            {
+                if (rogalik.transform.position.y > stopPosition)
+                {
+                    transform.position = new Vector3(rogalik.transform.position.x, rogalik.transform.position.y, -10f);
+                }
+            }
+        }
+    }
+}
---
+++ scripts/EventScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventScript : MonoBehaviour {
+
+
+    public bool isVisible;
+    public bool isDestroy;
+
+    public float waitTime = 1;
+
+    void Awake()
+    {
+        isVisible = false;
+        isDestroy = false;
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+        isDestroy = true;
+        isVisible = false;
+        Debug.Log("Destroy");
+    }
+
+    IEnumerator WaitAndSetIsVisible()
+    {
+        yield return new WaitForSeconds(waitTime);
+        isVisible = true;
+    }
+
+    void OnBecameVisible()
+    {
+        Debug.Log("Visible");
+        StartCoroutine(WaitAndSetIsVisible());
+    }
+
+    // Use this for initialization
+    void Start () {
+        var valTorque = Random.Range(100f, 200f);
+        gameObject.GetComponent<Rigidbody2D>().AddTorque(valTorque);
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+	    gameObject.transform.localScale += new Vector3(0.02F, 0.02F, 0);
+    }
+}
