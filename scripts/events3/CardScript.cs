e789b2d8c58e472b68813fde6b549949ee93779e
Alexander Chuvashov <chuvashovalexander@gmail.com>
2016-05-18 00:11:09 +0500

    Lesson 2

---
+++ scripts/events3/CardScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardScript : MonoBehaviour {
+
+    public Sprite onSprite;
+    public Sprite offSprite;
+
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void Awake()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Disable");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("Enable");
+    }
+
+    void OnMouseDown()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = onSprite;
+
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseUp()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = offSprite;
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 cursorPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 cursorPosition = Camera.main.ScreenToWorldPoint(cursorPoint) + offset;
+        transform.position = cursorPosition;
+    }
+
+    void OnCollisionEnter2D(Collision2D collider)
+    {
+        Debug.Log(collider.gameObject.name + " 1");
+    }
+
+    void OnCollisionExit2D(Collision2D collider)
+    {
+        Debug.Log(collider.gameObject.name + " 2");
+    }
+
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        Debug.Log(collider.gameObject.name + " 3");
+    }
+}
---
+++ scripts/events3/SecondEvents.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml.Serialization;
+
+public class SecondEvents : MonoBehaviour
+{
+
+    public bool status = true;
+    private GameObject[] cards;
+    void Awake()
+    {
+        cards = GameObject.FindGameObjectsWithTag("Card");
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 5, 100, 30), "Нажми меня!"))
+        {
+            status = !status;
+            //cards = GameObject.FindGameObjectsWithTag("Card");
+            foreach (GameObject card in cards)
+            {
+                card.active = status;
+            }
+        }
+
+        if (GUI.Button(new Rect(Screen.width / 2 - 50, 50, 100, 30), "Падаем!"))
+        {
+            
+            foreach (GameObject card in cards)
+            {
+                card.GetComponent<Rigidbody2D>().isKinematic = false;
+            }
+        }
+    }
+}
